0a95f869dbdef92eaae5a6b63b92e52438c69f2a
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-31 20:28:57 +0200

    Added GameObject class

---
+++ include/GameObject.hpp
@@ -0,0 +1,17 @@
+#ifndef DS_GAME_OBJECT_HPP
+#define DS_GAME_OBJECT_HPP
+
+#include <SFML/Graphics.hpp>
+#include "Updatable.hpp"
+
+namespace ds {
+    class GameObject : public Updatable {
+    private:
+        sf::IntRect rect;
+
+    public:
+        sf::IntRect getRect() const;
+    };
+}
+
+#endif // DS_GAME_OBJECT_HPP
---
+++ src/GameObject.cpp
@@ -0,0 +1,5 @@
+#include "../include/GameObject.hpp"
+
+sf::IntRect ds::GameObject::getRect() const {
+    return rect;
+}
