7d4c22f4f4d3104b392fe359914431df621ad5f3
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-31 23:43:17 +0200

    Added FontLoader class

---
+++ include/FontLoader.hpp
@@ -0,0 +1,17 @@
+#ifndef DS_FONT_LOADER_HPP
+#define DS_FONT_LOADER_HPP
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+namespace ds {
+    class FontLoader {
+    private:
+        std::map<std::string, sf::Font*> fontMap;
+    public:
+        FontLoader();
+        sf::Font* getFont(std::string path);
+    };
+}
+
+#endif // DS_FONT_LOADER_HPP
