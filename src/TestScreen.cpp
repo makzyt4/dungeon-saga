10c230b89008ba6d98392783394f466dd8ebe2b8
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-04-03 00:31:04 +0200

    Moved test text to left top corner

--- src/TestScreen.cpp
+++ src/TestScreen.cpp
@@ -41,8 +41,11 @@ void ds::TestScreen::update() {
 
     sf::Vector2i center = hero.getCenter();
 
-    sf::Vector2i textPos = sf::Vector2i(center.x - 100, center.y - 100);
+
+    sf::Vector2i textPos = sf::Vector2i(center.x - window->getSize().x / 4,
+                                        center.y - window->getSize().y / 4);
     text.setPosition(textPos);
+    printf("%d, %d\n", textPos.x, textPos.y);
 
     generateView(hero.getCenter());
     hero.handleKeys();
