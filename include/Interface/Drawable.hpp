69eac07d3b7c1eb13cff824b313f2613fe979f73
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-04-03 22:52:38 +0200

    Added Drawable interface

---
+++ include/Interface/Drawable.hpp
@@ -0,0 +1,11 @@
+#ifndef DS_INTERFACE_DRAWABLE_HPP
+#define DS_INTERFACE_DRAWABLE_HPP
+
+namespace ds {
+    class Drawable {
+    public:
+        virtual void draw() = 0;
+    };
+}
+
+#endif // DS_INTERFACE_DRAWABLE_HPP
