0b04d63902a1a83a53364eec1bfb0e6a297394a7
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-04-03 22:43:46 +0200

    Added ExitCode enum class

---
+++ include/Enum/ExitCode.hpp
@@ -0,0 +1,14 @@
+#ifndef DS_ENUM_EXITCODE_HPP
+#define DS_ENUM_EXITCODE_HPP
+
+namespace ds {
+    enum class ExitCode {
+        Running,
+        Quit,
+        Main,
+        Test,
+        Game
+    };
+}
+
+#endif // DS_ENUM_EXITCODE_HPP
