f8a1852395177eb654164ae1bf8b494f6ffe76d5
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-04-03 22:48:53 +0200

    Added Updatable interface

---
+++ include/Interface/Updatable.hpp
@@ -0,0 +1,11 @@
+#ifndef DS_INTERFACE_UPDATABLE_HPP
+#define DS_INTERFACE_UPDATABLE_HPP
+
+namespace ds {
+    class Updatable {
+    public:
+        virtual void update() = 0;    
+    };
+}
+
+#endif // DS_INTERFACE_UPDATABLE_HPP
