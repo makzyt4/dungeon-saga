1cb9b3ca7a26f39d84ea251ebd322cc6df60a920
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-04-03 23:10:52 +0200

    Added LookingDirection enum class

---
+++ include/Enum/LookingDirection.hpp
@@ -0,0 +1,11 @@
+#ifndef DS_ENUM_LOOKINGDIRECTION_HPP
+#define DS_ENUM_LOOKINGDIRECTION_HPP
+
+namespace ds {
+    enum class LookingDirection {
+        Left,
+        Right
+    };
+}
+
+#endif // DS_ENUM_LOOKINGDIRECTION_HPP
