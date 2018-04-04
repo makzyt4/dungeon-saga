84a47b1ff074997d10eae191b5e72f73bdaa6d45
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-04-04 16:55:56 +0200

    Added MenuState enum class

---
+++ include/Enum/MenuState.hpp
@@ -0,0 +1,14 @@
+#ifndef DS_ENUM_MENUSTATE_HPP
+#define DS_ENUM_MENUSTATE_HPP
+
+namespace ds {
+    enum class MenuState {
+        Normal,
+        Highlighted,
+        Clicked,
+        Checked,
+        Unchecked
+    };
+}
+
+#endif // DS_ENUM_MENUSTATE_HPP
